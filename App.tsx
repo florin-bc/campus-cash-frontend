e5c1e8186b222cccc54f3ee5c6c6c45986474c64
Micu Rares <micu_rares04@yahoo.com>
2023-04-09 04:42:07 +0300

    temp

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Profile from "./screens/Profile";
 import RootNavigator from "./src/router";
 
 export default function App() {
---
+++ screens/Profile.js
@@ -0,0 +1,31 @@
+import ProfileCircle from "../components/ProfileCircle";
+import { StyleSheet, View } from "react-native";
+
+export default function Profile() {
+  return (
+    <View>
+      <View style={styles.back} />
+      <View style={styles.container}>
+        <ProfileCircle />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#242423",
+    width: "100%",
+    height: "100%",
+  },
+  back: {
+    marginTop: 12,
+    marginLeft: 12,
+    height: 24,
+    width: 24,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 12,
+  }
+});
